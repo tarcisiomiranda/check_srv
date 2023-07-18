93bde47665769186c702929b94b0099d57e87d9e
Tarcisio Miranda <eu@tarcisio.me>
2023-07-17 21:46:30 -0300

    programa finalizado, testando no centos 7

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 ## How to run
+
+***Clone project***
+```
+git clone link.git /opt/check_srv
+```
+
 ***Run command line***
 ```
-python3 /caminho/para/check_srv.py --vg=docker --lv=srv
+python3 /opt/check_srv/check_srv.py --vg=docker --lv=srv
 ```
 
 ***Run every 6 hour***
 ```
-0 */6 * * * python3 /caminho/para/check_srv.py.py --vg=docker --lv=srv >> /var/log/check_srv.log 2>&1
+0 */4 * * * root /usr/bin/python3 /opt/check_srv/check_srv.py --vg=docker --lv=srv >> /var/log/check_srv.log 2>&1
 ```
--- check_srv.py
+++ check_srv.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import argparse
+from datetime import datetime
 
 def check_partition():
     df_output = subprocess.check_output(["df", "/srv"]).decode("utf-8")
@@ -10,18 +11,35 @@ def check_partition():
     return usage_percent
 
 def extend_volume(vg_name, lv_name):
-    subprocess.run(["lvextend", "-L", "+1G", f"/dev/{vg_name}/{lv_name}"])
-    subprocess.run(["resize2fs", f"/dev/{vg_name}/{lv_name}"])
+    def run_cmmd(cmmd):
+        proc = subprocess.Popen(cmmd, stdout=subprocess.PIPE, \
+        stderr=subprocess.PIPE, universal_newlines=True)
+        out, err = proc.communicate()
+        return [out.strip(), err.strip()]
+
+    try:
+        cmmd = ["lvextend", "-L", "+1G", f"/dev/{vg_name}/{lv_name}", "--resize"]
+        res = run_cmmd(cmmd)
+        if 'free space' in res[1].lower():
+            return f'Insufficient free space in VG: {vg_name}'
+        else:
+            return f'Logical Volume extended successfully, {lv_name} with {check_partition()}%.'
+
+    except subprocess.CalledProcessError as e:
+        print(f"Except, an error extending Logical Volume: {e}")
+        return(f"Except, an error extending Logical Volume: {e}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Extend Logical Volume if partition usage is above 99%.")
+    parser = argparse.ArgumentParser(description="Extend Logical Volume if partition usage is above 98%.")
     parser.add_argument("--vg", help="Volume Group name", required=True)
     parser.add_argument("--lv", help="Logical Volume name", required=True)
     args = parser.parse_args()
 
     partition_usage = check_partition()
     if partition_usage >= 98:
-        extend_volume(args.vg, args.lv)
-        print("Logical Volume extended successfully.")
+        res = extend_volume(args.vg, args.lv)
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{current_time}] {res}")
     else:
-        print("Partition below 99%. No extension required.")
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{current_time}] Partition {partition_usage}%, below of 98%. No extension required.")
